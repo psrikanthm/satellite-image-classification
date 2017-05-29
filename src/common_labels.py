a846e1fa1b5245269bada1c8549fc47aac14cd6b
srikanth <srikanth@debian>
2017-05-29 16:28:38 -0400

    handling multilabels

--- src/common_labels.py
+++ src/common_labels.py
@@ -10,19 +10,40 @@ TRAIN_TIFF = "../data/train-tif-v2"
 
 ##################################
 
+def encode2label(array_x):
+    a = []
+    for x in array_x:
+        a.append(sum(1<<i for i, b in enumerate(x[::-1]) if b))
+    
+    a = np.array(a)
+    import sklearn.preprocessing
+    label_binarizer = sklearn.preprocessing.LabelBinarizer()
+    label_binarizer.fit(range(max(a)+1))
+    return label_binarizer.transform(a)
+
+
+
 common_labels = ["primary", "water", "habitation", "agriculture", "road", "cultivation", "bare_ground"]
 Y = py.selected_columns(common_labels)
 Y = Y[(Y.T != 0).any()]
 
 X = []
-for i in Y.index:
-    print "Read Image: ", i
-    img = cv2.imread(TRAIN_TIFF + '/train_'+ str(i) +'.tif')
-    X.append(img)
+#for i in Y.index:
+#    print "Read Image: ", i
+#    img = cv2.imread(TRAIN_TIFF + '/train_'+ str(i) +'.tif')
+#    X.append(img)
 
 X = np.array(X)
+Y = Y.as_matrix()
+print Y.shape
+print Y[1,:]
+print Y[3,:]
+Y = encode2label(Y)
+print np.where(Y[1,:] == 1)
+print np.where(Y[3,:] == 1)
 
-np.save('../data/x_train_cl.npy', X)
+
+#np.save('../data/x_train_cl.npy', X)
 np.save('../data/y_train_cl.npy', Y)
 
 print X.shape
--- src/test_pred.py
+++ src/test_pred.py
@@ -13,22 +13,48 @@ y2 = np.load('../data/y_test_weather.npy')
 
 names = np.load('../data/test_filenames.npy')
 
+def encode2binary(array_x):
+    a = []
+    for x in array_x:
+        a.append([int(i) for i in bin(x)[2:]])
+    return np.array(a)
 
-def get_common_labels(y1[i]):
-    return []
+def softmax(x):
+    x = np.transpose(x)
+    scoreMatExp = np.exp(np.asarray(x))
+    s = scoreMatExp / scoreMatExp.sum(0)
+    return np.transpose(s)
 
-#ofile  = open('prediction.csv', "wb")
-#writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-     
-for i in range(len(y2)):
+y1 = softmax(y1)
+y2 = softmax(y2)
+
+w1 = encode2binary(np.argmax(y1, axis = 1))
+w2 = np.argmax(y2, axis = 1)
+
+ofile  = open('../data/prediction.csv', "wb")
+writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    
+row = []
+row.append('image_name')
+row.append('tags')
+writer.writerow(row)
+ 
+for i in range(len(w2)):
     row = []
     filename = os.path.splitext(names[i])[0]
+
     row.append(filename)
-    wr_label = np.argmax(y2)[0]
-    c_labels = ''
-    c_classes = get_common_labels(y1[i])
+
+    tags =  weather_labels[w2[i]]
     
-    row.append(wr_label + " " + c_labels)
-    #writer.writerow(row)
+    if w2[i] != 0: 
+        clabels = np.where(w1[i,:] == 1)[0]
+        for label in clabels:
+            if label == 1:
+                print common_labels[label]
+            tags += " " + common_labels[label]
+    tags = tags.strip()
+    row.append(tags)
+    writer.writerow(row)
  
-#ofile.close()
+ofile.close()
